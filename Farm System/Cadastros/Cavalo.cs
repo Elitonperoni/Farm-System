efc95a3a3e1e59784fceb6649fbd6f4a181be17c
Eliton Peroni <eliton_peroni@hotmail.com>
2023-01-15 11:04:43 -0300

    Adicionar arquivos de projeto.

---
+++ Farm System/Cadastros/Cavalo.cs
@@ -0,0 +1,42 @@
+﻿using Farm_System.Model;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm_System.Cadastros
+{
+    public class Cavalo
+    {     
+        Conexao conexao = new Conexao();
+        NpgsqlCommand cmd = new NpgsqlCommand();
+        public String mensagem = "";
+       
+        public Cavalo(String nome, string raca)
+        {
+            cmd.CommandText = "INSERT INTO CAVALOS(NOME, RACA) VALUES(@nome, @raca)";
+
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@raca", raca);
+
+            try
+            {
+                cmd.Connection = conexao.Conectar();
+                cmd.ExecuteNonQuery();
+                conexao.Desconectar();
+
+                this.mensagem = "Cadastrado com sucesso";
+
+            } catch( SqlException e)
+            {
+                this.mensagem = "Erro ao conectar-se ao banco de dados";
+            }
+
+        }
+        
+    }
+}
---
+++ Farm System/Model/Conexao.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm_System.Model
+{
+    public class Conexao
+    {
+        NpgsqlConnection con = new NpgsqlConnection();
+        public Conexao() 
+        {
+            con.ConnectionString = "server=localhost;port=5432;user id=postgres;password=123456;database=farmsystem";
+        }    
+
+        public NpgsqlConnection Conectar()
+        {
+            if (con.State == System.Data.ConnectionState.Closed) {
+                con.Open();
+            }
+            return con;
+        }
+
+        public void Desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }           
+        }
+    }
+}
